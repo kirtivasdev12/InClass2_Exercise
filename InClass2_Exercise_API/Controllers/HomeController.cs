ed906be2d8eaa229a44620a9b3d563cd22b25cd6
bibhas05041984 <bibhas.apr5@gmail.com>
2020-07-03 18:34:49 -0400

    * .gitignore: * InClass2_Exercise_API.sln: * Program.cs: * Startup.cs: * Parks.cs: * appsettings.json: * site.js: * favicon.ico: * site.css: * Index.cshtml: * _ViewStart.cshtml: * ErrorViewModel.cs: * MyLInk.cshtml: * _ViewImports.cshtml: * Privacy.cshtml: * Error.cshtml: * _Layout.cshtml: * appsettings.Development.json: * InClass2_Exercise_API.csproj: * HomeController.cs: * LICENSE: * LICENSE.txt: * launchSettings.json: * jquery.js: * jquery.min.js: * jquery.min.map: * LICENSE.md: * bootstrap.js: * bootstrap.css: * _ValidationScriptsPartial.cshtml: * bootstrap.js.map: * bootstrap.min.js: * bootstrap.min.css: * bootstrap.css.map: * bootstrap-grid.css: * bootstrap.bundle.js: * bootstrap.min.js.map: * bootstrap-reboot.css: * bootstrap.min.css.map: * LICENSE.txt: * bootstrap-grid.min.css: * bootstrap.bundle.js.map: * bootstrap-grid.css.map: * bootstrap.bundle.min.js: * jquery.validate.js: * bootstrap-reboot.css.map: * additional-methods.js: * jquery.validate.min.js: * bootstrap.bundle.min.js.map: * bootstrap-grid.min.css.map: * bootstrap-reboot.min.css.map: * additional-methods.min.js: * jquery.validate.unobtrusive.js: * jquery.validate.unobtrusive.min.js:
    * bootstrap-reboot.min.css: Commit In Class Assignment 1 and

---
+++ InClass2_Exercise_API/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+using InClass2_Exercise_API.Models;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+
+namespace InClass2_Exercise_API.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        HttpClient httpClient;
+
+        static string BASE_URL = "https://developer.nps.gov/api/v1/";
+        static string API_KEY = "36KAHyxFO1wZnsVtYITsJNm3pCLeSoUalsdo6aSx"; // Bibhas Key
+        public IActionResult Index()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            string NATIONAL_PARK_API_PATH = BASE_URL + "/parks?limit=10";
+            string parksData = "";
+
+            Parks parks = null;
+
+            httpClient.BaseAddress = new Uri(NATIONAL_PARK_API_PATH);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(NATIONAL_PARK_API_PATH).GetAwaiter().GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    parksData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!parksData.Equals(""))
+                {
+                    
+                    parks = JsonConvert.DeserializeObject<Parks>(parksData);
+                }
+            }
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+            return View(parks);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult MyLink()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
+
+
